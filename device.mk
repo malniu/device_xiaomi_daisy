ad1db64b3b332cb15f1af55365f4e390dc407671
SebaUbuntu <barezzisebastiano@gmail.com>
2020-12-22 13:50:28 +0100

    daisy: build AOSP light HIDL

--- device.mk
+++ device.mk
@@ -261,7 +261,8 @@ PRODUCT_PACKAGES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.xiaomi_daisy
+    android.hardware.light@2.0-impl \
+    android.hardware.light@2.0-service
 
 # Media
 PRODUCT_COPY_FILES += \
