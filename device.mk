128f3bc4f597006e9c00b3ce8acf9f8db85b5c99
SebaUbuntu <barezzisebastiano@gmail.com>
2020-12-22 13:47:26 +0100

    daisy: build AOSP IR HIDL

--- device.mk
+++ device.mk
@@ -145,9 +145,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/component-overrides.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sysconfig/component-overrides.xml
 
-# Consumerir
+# ConsumerIr
 PRODUCT_PACKAGES += \
-    android.hardware.ir@1.0-service.xiaomi_daisy
+    android.hardware.ir@1.0-impl \
+    android.hardware.ir@1.0-service
 
 # Display
 PRODUCT_PACKAGES += \
