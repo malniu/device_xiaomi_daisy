9c9770b18178f09fd36cd3d6e5f63a2a31465410
a-huk <56552991+a-huk@users.noreply.github.com>
2021-01-26 16:54:34 +0100

    Introduce OTA updates

--- lineage_daisy.mk
+++ lineage_daisy.mk
@@ -41,5 +41,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "google/sunfish/sunfish:11/RQ1A.201205.008/6943376:user/release-keys"
 
+# Lineage Updater
+PRODUCT_PROPERTY_OVERRIDES += \
+    lineage.updater.uri=https://raw.githubusercontent.com/a-huk/otaserver/master/1800-01.json
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.build.fingerprint=$(BUILD_FINGERPRINT)
